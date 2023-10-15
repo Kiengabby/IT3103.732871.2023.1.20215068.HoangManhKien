8e442cdf5b939dc296aac988617044121c7ae359
Kiengabby <146078815+Kiengabby@users.noreply.github.com>
2023-10-16 01:36:06 +0700

    Delete FirstDialog.java

--- FirstDialog.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hoang Manh Kien - 20215068 - Hello world! How are you?");
-        System.exit(0);
-    }
-}
